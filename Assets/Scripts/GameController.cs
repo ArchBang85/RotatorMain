965072852c9d59cf607a4036dac49f81e8f5f3da
Petri Autio <archbang85@gmail.com>
2016-03-06 15:21:43 +0000

    Kaleidoscope effect, rotating main wheel, texts

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject outerWheel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.A))
+        {
+            outerWheel.transform.Rotate(Vector3.forward * Time.deltaTime * 10);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            outerWheel.transform.Rotate(Vector3.forward * Time.deltaTime * -10);
+        }
+	
+	}
+}
